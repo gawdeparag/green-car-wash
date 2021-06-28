1ff41d3fb646609b3260a1cbbb27b33882f973da
Parag G <parag.gawde@capgemini.com>
2021-06-28 22:33:30 +0530

    feat: Add signup-login for admin service

--- gcw-backend/gcw-admin/main.js
+++ gcw-backend/gcw-admin/main.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const adminRoutes = require('./routers/admin');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 const { MongoURL } = require('./URL');
+const { requireAuth, checkUser } = require('./middleware/adminAuthMiddleware');
+const cors = require('cors');
 
 var PORT = process.env.PORT || 3000;
 
@@ -11,8 +15,24 @@ mongoose.connect(MongoURL, () => {
 });
 mongoose.Promise = global.Promise;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(cookieParser());
+
+app.get('/profile', requireAuth, (req, res) => { res.render('profile') });
 app.use(adminRoutes);
 
+var corsOpt = {
+    origin: process.env.CORS_ALLOW_ORIGIN || '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOpt));
+app.options('*', cors(corsOpt));
+
 app.listen(PORT, () => {
-    console.log(`Admin Service started at ${PORT}`);
+    console.log(`Admin Service Started at ${PORT}`);
+    app.get("/", (req, res) => {
+        res.send("NGINX API Gateway connected successfully");
+    });
 });
