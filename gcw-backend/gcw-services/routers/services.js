bb90a4d453e1e411752f26f841bd4ea3bae47df2
Parag G <parag.gawde@capgemini.com>
2021-07-02 17:31:58 +0530

    refactor: Add correct path to auth middleware

--- gcw-backend/gcw-services/routers/services.js
+++ gcw-backend/gcw-services/routers/services.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const serviceController = require('../controllers/serviceController');
-const userAuthMiddleware = require('../../gcw-user/middleware/userMiddleware');
+const userAuthMiddleware = require('../../gcw-user/middleware/authMiddleware');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
