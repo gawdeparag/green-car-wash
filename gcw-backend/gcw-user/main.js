adb88f0858d71d519c9e6f2d5ef32c5b53369d0f
Parag G <parag.gawde@capgemini.com>
2021-07-03 00:51:49 +0530

    feat: Add swagger to gcw-user

--- gcw-backend/gcw-user/main.js
+++ gcw-backend/gcw-user/main.js
@@ -4,6 +4,9 @@ const userRoutes = require('./routers/user');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { MongoURL } = require('./URL');
+const swaggerDocument = require('./swagger.json');
+const swaggerUI = require('swagger-ui-express');
+const cors = require('cors');
 
 var PORT = process.env.PORT || 3005;
 
@@ -11,6 +14,15 @@ mongoose.connect(MongoURL, () => {
     console.log("Connected to DB gwc-user");
 });
 mongoose.Promise = global.Promise;
+
+var corsOpt = {
+    origin: process.env.CORS_ALLOW_ORIGIN || '*', // this work well to configure origin url in the server
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
+    allowedHeaders: ['Content-Type', 'Authorization'] // allow json and token in the headers
+};
+app.use(cors(corsOpt)); // cors for all the routes of the application
+app.options('*', cors(corsOpt));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(cookieParser());
 
