c447088b0b9b564513da45feffe95693e4be7ac7
Parag G <parag.gawde@capgemini.com>
2021-07-02 17:24:11 +0530

    feat: Add auth middleware to gcw-services

--- gcw-backend/gcw-services/main.js
+++ gcw-backend/gcw-services/main.js
@@ -6,9 +6,19 @@ const addOnRoutes = require('./routers/addOns');
 const { MongoURL } = require('./URL');
 const swaggerDocument = require('./swagger.json');
 const swaggerUI = require('swagger-ui-express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 var PORT = process.env.PORT || 3004;
+var corsOpt = {
+    origin: process.env.CORS_ALLOW_ORIGIN || '*', // this work well to configure origin url in the server
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
+    allowedHeaders: ['Content-Type', 'Authorization'] // allow json and token in the headers
+};
+app.use(cors(corsOpt)); // cors for all the routes of the application
+app.options('*', cors(corsOpt));
 
+app.use(cookieParser());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 mongoose.connect(MongoURL, () => {
